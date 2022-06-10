46d152ff49c16e814454b6a83554875e63f30dd4
David <Rigby335@hotmail.com>
2022-06-10 09:23:54 -0700

    tree image renders and buttons work

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import { useState } from 'react';
 function App() {
 
   // const [animals, setAnimals] = useState (10);
-  const [catSize, setCatSize] = useState(10)
-;  // function handleCat() {
+  // const [catSize, setCatSize] = useState(10);
+
+  const [plantSize, setPlantSize] = useState(10);
+  const [weedSize, setWeedSize] = useState(10);
+
+  // function handleCat() {
   //   animals.push('cat');
 
   //   setAnimals(animals.slice());
@@ -13,10 +17,17 @@ function App() {
 
   return (
     <div className="App">
-      <div className='animals'>
-        <p style={{ fontSize: `${catSize}px` }}>🐱 : {catSize}</p>
+      <div className='plants'>
+        <div className='smalltree'>
+          <img src="smalltree.png" width={10 * plantSize} />
+        </div>
+        {/* <p style={{ fontSize: `${catSize}px` }}>🐱 : {catSize}</p>
         <button onClick={() => setCatSize(catSize + 10)}>Grow cat</button>
-        <button onClick={() => setCatSize(catSize - 10)}>Shrink cat</button>
+        <button onClick={() => setCatSize(catSize - 10)}>Shrink cat</button> */}
+        <div className='buttons'>
+          <button onClick={() => setPlantSize(plantSize + 1)}>Water the tree to make it grow!</button>
+          <button onClick={() => setPlantSize(plantSize - 1)}>Oh no! The tree is too big! Shrink it!</button>
+        </div>
       </div>
     </div>
   );
