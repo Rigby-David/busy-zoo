768c3d665982f1dccd4736798dd671ee34253fdd
David <Rigby335@hotmail.com>
2022-06-30 12:28:34 -0700

    App.test testing button onClick

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  // render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+test('if the user clicks on hammer, another hammer should render on the screen', () => {
+  render(<App />);
+  const hammerOnLoad = screen.getAllByText(/🔨/i);
+
+  expect(hammerOnLoad.length).toBe(1);
+
+  const hammerButton = screen.getByText(/Add a hammer!/i);
+
+  fireEvent.click(hammerButton);
+
+  const hammerAfterClick = screen.getAllByText(/🔨/i);
+
+  expect(hammerAfterClick.length).toBe(2);
+});
+
+test('if the user clicks the axe button, add another axe to the screen', () => {
+  render(<App />);
+
+  const axeOnLoad = screen.getAllByText(/🪓/i); 
+
+  expect(axeOnLoad.length).toBe(1);
+
+  const axeButton = screen.getByText(/Add an axe!/i);
+
+  fireEvent.click(axeButton);
+
+  const axeAfterClick = screen.getAllByText(/🪓/i);
+
+  expect(axeAfterClick.length).toBe(2);
 });
